7fc02d0fd4e742111449da185ab85ba8314b44b4
stevengirving <sgirving@outlook.com>
2023-03-05 23:12:11 -0700

    Initial commit

---
+++ README.md
@@ -0,0 +1,6 @@
+# Truth Quiz
+
+Simple script that will iterate through a list of entries to determine
+the truth value of each entry, then quiz the user on the truth value.
+
+Tallies up total correct answers at the end.
---
+++ truth_quiz.py
@@ -0,0 +1,48 @@
+questions = (
+    (r'True and True'), # True
+    (r'False and True'), # False
+    (r'1 == 1 and 2 == 1'), # False
+    (r'"test" == "test"'), # True
+    (r'1 == 1 or 2 != 1'), # True
+    (r'True and 1 == 1'), # True
+    (r'False and 0 != 0'), # False
+    (r'True or 1 == 1'), # True
+    (r'False and 0 != 0'), # False
+    (r'True or 1 == 1'), # True
+    (r'"test" == "testing"'), # False
+    (r'1 != 0 and 2 == 1'), # False
+    (r'"test" != "testing"'), # True
+    (r'"test" == 1'), # False
+    (r'not (True and False)'), # True
+    (r'not (1 == 1 and 0 != 1)'), # False
+    (r'not (10 == 1 or 1000 == 1000)'), # False
+    (r'not (1 != 10 or 3 == 4)'), # False
+    (r'not ("testing" == "testing" and "Zed" == "Cool Guy")'), # True
+    (r'1 == 1 and (not ("testing" == 1 or 1 == 0))'), # True
+    (r'"chunky" == "bacon" and (not (3 == 4 or 3 == 3))'), # False
+    (r'3 == 3 and (not ("testing" == "testing" or "Python" == "Fun"))') # False
+    )
+
+# Define function to pull question from `questions`
+    # Print the question
+    # Get user input for answer
+    # Compare Answer to Question
+    # Print Correct or Wrong
+
+def truth_questions():
+    correct = []
+    for question in questions:
+        print(f"{question}: True or False?")
+        answer = input("> ")
+        print(f"You wrote: {answer}")
+        truth = str(eval(question))
+        print(f"The answer was: {truth}")
+        if truth == answer:
+            print("You were right!")
+            correct.append("1")
+        else:
+            print("You were incorrect!")
+            correct.append("0")
+        print("---")
+    print("You got", correct.count("1"), "of", len(correct), "correct!")
+
